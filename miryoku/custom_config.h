0ea7eeb240e491bfb91f46b98681a4da707aff0d
Jan Skoda <jan.skoda@liquiditylabs.xyz>
2026-03-11 19:14:38 +0100

    fix doubletap error, ci exposes if2 files directly

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Add debounce configuration to prevent multiple key registrations
+#define CONFIG_ZMK_KSCAN_DEBOUNCE_PRESS_MS 20
+#define CONFIG_ZMK_KSCAN_DEBOUNCE_RELEASE_MS 300
+
