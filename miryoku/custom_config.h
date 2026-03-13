b80dc646b510c56e5d7475f78f736e478e2fe112
Jan Skoda <jan.skoda@liquiditylabs.xyz>
2026-03-13 21:10:04 +0100

    revert doubletap fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-// Add debounce configuration to prevent multiple key registrations
-#define CONFIG_ZMK_KSCAN_DEBOUNCE_PRESS_MS 20
-#define CONFIG_ZMK_KSCAN_DEBOUNCE_RELEASE_MS 300
-
