f16e2d83b1e92c5e7e46c373ec30f800048eb366
Jan Skoda <jan.skoda@liquiditylabs.xyz>
2026-03-20 22:35:11 +0100

    fix desktop switching

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -273,9 +273,9 @@ U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_NAV \
-&kp LA(F5),        &kp LA(F6),        &kp LA(F7),        &kp LA(F8),        U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LC(F1),        &kp LC(F2),        &kp LC(F3),        &kp LC(F4),        U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
-&kp LS(LC(F1)),    &kp LS(LC(F2)),    &kp LS(LC(F3)),    &kp LS(LC(F4)),    U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
+&kp LC(F1),        &kp LC(F2),        &kp LC(F3),        &kp LC(F4),        U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
+&kp LA(F5),        &kp LA(F6),        &kp LA(F7),        &kp LA(F8),        U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 
